2d9cd8cd047dabacabfccd542433b78e39a62396
Lucas Cardoso <lucasreis-@outlook.com>
2023-11-01 15:07:09 -0300

    adicionando desafio I -jogo da velha

---
+++ Desafios/Desafio I/Extras/JogoDaForca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_I.Extras
+{
+    internal class JogoDaForca
+    {
+        public static void principal()
+        {
+            desenhoForca();
+        }
+
+        static void desenhoForca()
+        {
+            Console.WriteLine("+---+");
+            Console.WriteLine("|   |");
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            Console.WriteLine("=========");
+        }
+    }
+}
