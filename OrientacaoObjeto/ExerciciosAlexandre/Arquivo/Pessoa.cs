7fa591c0b556cc2a8a2bc60cadaf6ea533d942ff
Lucas Cardoso <lucasreis-@outlook.com>
2023-11-04 12:46:49 -0300

    manipulação de arquivos e coleções

---
+++ OrientacaoObjeto/ExerciciosAlexandre/Arquivo/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivo
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Pessoa(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome + " : " + Email;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Pessoa pessoa && Email == pessoa.Email;
+        }
+    }
+}
---
+++ OrientacaoObjeto/ExerciciosAlexandre/Arquivo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Arquivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string nome, email;
+            List<Pessoa> lista = new List<Pessoa>();
+            Persistencia.popularArquivoLista("alunos.csv", lista); //conexao com a base
+
+            Persistencia.exibirLista(lista);
+
+            Pessoa tmp;
+            do
+            {
+                Console.Write("Digite nome ou sair para finalizar: ");
+                nome = Console.ReadLine().ToUpper();
+                if (nome == "SAIR")
+                {
+                    break;
+                }
+                Console.Write("Digite email: ");
+                email = Console.ReadLine().ToLower();
+                tmp = new Pessoa(nome, email);
+
+                if (lista.Contains(tmp))
+                {
+                    Console.WriteLine("Email já em uso!!");
+                }
+                else
+                {
+                    lista.Add(tmp);
+                    Persistencia.gravarSimples(tmp, "alunos.csv");
+                }
+            } while (true);
+
+        }
+    }
+}
