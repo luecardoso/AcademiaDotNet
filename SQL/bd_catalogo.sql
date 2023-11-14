cfb06b8c368b1e62736557f556bd5dce9337e34e
Lucas Cardoso <lucasreis-@outlook.com>
2023-11-14 12:44:48 -0300

    avaliação de exercícios de javascript

--- SQL/bd_catalogo.sql
+++ SQL/bd_catalogo.sql
@@ -4,37 +4,56 @@ use catalogo;
 
 create table grupo
 (
-	ID integer not null primary key,
-	nome varchar(20) not null
-)
+	id integer not null primary key identity,
+	nome varchar(30) not null
+);
 
 create table cientista
 (
-	ID integer not null primary key,
+	id integer not null primary key identity,
 	nome varchar(50) not null,
 	nacionalidade varchar(35)
-)
+);
 
-/*
 create table era
 (
-	ID integer not null primary key,
+	id integer not null primary key identity,
 	nome varchar(20) not null,
 	inicio_era integer,
 	fim_era integer
-)
-*/
+);
+
 create table dinossauro
 (
+	ID integer not null primary key identity,
 	nome varchar(50) not null,
 	toneladas integer,
 	ano_descoberta varchar(4) not null,
 	pais_descoberto varchar(20),
+	/*
 	era varchar(20) not null,
 	inicio_era integer,
 	fim_era integer,
+	*/
 	fk_grupo integer not null,
-	foreign key(fk_grupo) references grupo(ID),
+	foreign key(fk_grupo) references grupo(id),
 	fk_descobridor integer not null,
-	foreign key(fk_descobridor) references cientista(ID)
-)
+	foreign key(fk_descobridor) references cientista(id),
+	fk_era integer not null,
+	foreign key(fk_era) references era(id)
+);
+
+insert into grupo (nome)
+values ('Anquilossauros'),
+		('Ceratopsídeos'),
+		('Estegossauros'),
+		('Terápodes');
+
+select * from grupo;
+
+insert into era (nome,inicio_era, fim_era)
+values ('Cretácio',145,63),
+		('Jurássico',205,142);
+
+
+select * from era;
